01d0efcc537464fd27b40cdb3bf1b345eaeb0f2f
Valeriano <jneto3000@gmail.com>
2019-11-11 18:36:56 +0000

    Revert "X00T: Add Lineage Touch HAL"
    This reverts commit 3167a22660422fe1f83650ae13778602ccc19f5c.

--- device.mk
+++ device.mk
@@ -407,10 +407,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     libtinyxml2
 
-# Touch HAL
-PRODUCT_PACKAGES += \
-    vendor.lineage.touch@1.0
-
 # USB
 PRODUCT_PACKAGES += \
 	android.hardware.usb@1.0-service.basic
